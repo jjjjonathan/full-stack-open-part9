a27e88695718b346ae1d993dcbadf811d77aa7ac
jjjjonathan <hornathanjon@gmail.com>
2021-04-27 22:22:08 -0500

    exercise 9.9 complete

--- patientor/index.ts
+++ patientor/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import cors from 'cors';
+
 const app = express();
 app.use(express.json());
+app.use(cors()); // eslint-disable-line
 
-const PORT = 3000;
+const PORT = 3001;
 
-app.get('/ping', (_request, response) => {
+app.get('/api/ping', (_request, response) => {
   console.log('ping!');
   response.send('pong!');
 });

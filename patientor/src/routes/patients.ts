38528cc4b2e5e6551b85b776c7322e6654628aa0
jjjjonathan <hornathanjon@gmail.com>
2021-05-05 21:30:48 -0500

    exercise 9.16 complete

--- patientor/src/routes/patients.ts
+++ patientor/src/routes/patients.ts
@@ -9,6 +9,19 @@ router.get('/', (_request, response) => {
   response.send(patientService.getSafeEntries());
 });
 
+router.get('/:id', (request, response) => {
+  const allPatients = patientService.getEntries();
+  const patient = allPatients.find(
+    (patient) => request.params.id === patient.id
+  );
+  const patientWithEntries = {
+    ...patient,
+    entries: [],
+  };
+
+  response.send(patientWithEntries);
+});
+
 router.post('/', (request, response) => {
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   const body: Omit<Patient, 'id'> = request.body;
